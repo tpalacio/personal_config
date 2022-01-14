6658bc60cb70ae12169e959687cfd6afb02d1eb5
Thoren Palacio <tjpalacio@gmail.com>
2022-01-14 02:55:53 +0000

    feat: add script to retrieve bigquery table desciptions

---
+++ home_dir/bigquery_describe_table.sh
@@ -0,0 +1,7 @@
+clear && read -p "Table name?
+" TABLENAME && echo -e $TABLENAME":" > ~/bigquery_describe_table.tmp && \
+echo "select c, data_type from system.columns where st = '$TABLENAME';" > table_name.tmp && \
+bq query --use_legacy_sql=false < table_name.tmp \
+>> ~/bigquery_describe_table.tmp && clear && less ~/bigquery_describe_table.tmp ;
+rm ~/postgres_describe_table.tmp ;
+rm ~/table_name.tmp
