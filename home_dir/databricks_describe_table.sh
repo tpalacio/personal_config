aef834087b4c2d2afb255e16ce3555083214ebaa
Thoren Palacio <tjpalacio@gmail.com>
2022-08-04 09:59:12 -0400

    Add script to describe databricks table

---
+++ home_dir/databricks_describe_table.sh
@@ -0,0 +1,5 @@
+clear && read -p "Table name?
+" TABLENAME && echo -e $TABLENAME":" > ~/databricks_describe_table.tmp && \
+databricks-sql-cli -e "DESCRIBE TABLE EXTENDED $TABLENAME" --table-format psql >> databricks_describe_table.tmp && \
+clear && less ~/databricks_describe_table.tmp ;
+rm ~/databricks_describe_table.tmp ;
