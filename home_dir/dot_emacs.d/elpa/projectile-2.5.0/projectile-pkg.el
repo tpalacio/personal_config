5c8b98d5bd4e21aa47792566e069539ecf29270d
Thoren Palacio <tjpalacio@gmail.com>
2022-10-09 20:42:14 -0400

    fix: update elpa packages

---
+++ home_dir/dot_emacs.d/elpa/projectile-2.5.0/projectile-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from projectile.el  -*- no-byte-compile: t -*-
+(define-package "projectile" "2.5.0" "Manage and navigate projects in Emacs easily" '((emacs "25.1")) :keywords '("project" "convenience") :authors '(("Bozhidar Batsov" . "bozhidar@batsov.dev")) :maintainer '("Bozhidar Batsov" . "bozhidar@batsov.dev") :url "https://github.com/bbatsov/projectile")
