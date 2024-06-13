63a3db18ebb9acbbf32484256be1d266d7294807
Thoren Palacio <tjpalacio@gmail.com>
2024-06-13 09:42:51 -0400

    feat: disable mouse for pager

--- home_dir/postgres_active_queries.sh
+++ home_dir/postgres_active_queries.sh
@@ -3,4 +3,4 @@ source ~/.aliases
 
 dw_root -X -c "select * from dbt_admin.active_queries;" > active_query_results.txt
 
-pspg active_query_results.txt
+pspg --no-mouse active_query_results.txt
--- home_dir/postgres_describe_table.sh
+++ home_dir/postgres_describe_table.sh
@@ -11,7 +11,7 @@ clear && read -p "Table name?
 " > table_name.tmp \
 && dw_root -X --csv -t -f table_name.tmp >> ~/postgres_describe_table.csv \
 && clear \
-&& pspg --csv --csv-header on ~/postgres_describe_table.csv ;
+&& pspg --csv --csv-header on --no-mouse ~/postgres_describe_table.csv ;
 rm ~/postgres_describe_table.csv ;
 rm ~/table_name.tmp
 # && dw_root -X -t -f table_name.tmp >> ~/postgres_describe_table.csv \
