a38f6658c622b72355538e5400ff3204952c6cab
Thoren Palacio <tjpalacio@gmail.com>
2024-11-13 14:42:46 -0500

    feat: make address explicit

--- home_dir/postgres_active_queries.sh
+++ home_dir/postgres_active_queries.sh
@@ -1,6 +1,6 @@
 shopt -s expand_aliases
 source ~/.aliases
 
-dw_root -X -c "select * from dbt_admin.active_queries;" > active_query_results.txt
+dw_root -X -c "select * from dbt_admin.active_queries;" > ~/active_query_results.txt
 
-pspg --no-mouse active_query_results.txt
+pspg --no-mouse ~/active_query_results.txt
--- home_dir/postgres_describe_table.sh
+++ home_dir/postgres_describe_table.sh
@@ -8,10 +8,10 @@ clear && read -e -p "Table name?
     select column_name, data_type from dbt_prod.unbound_views where st = replace('$TABLENAME', chr(34), '') order by ordinal_position;
     select column_name, data_type from dbt_prod.bound_views where st = replace('$TABLENAME', chr(34), '') order by ordinal_position;
     select column_name, data_type from dbt_prod.external_views where st = replace('$TABLENAME', chr(34), '') order by ordinal_position;
-" > table_name.tmp \
-&& dw_root -X --csv -t -f table_name.tmp >> ~/postgres_describe_table.csv \
+" > ~/table_name.tmp \
+&& dw_root -X --csv -t -f ~/table_name.tmp >> ~/postgres_describe_table.csv \
 && clear \
 && pspg --csv --csv-header on --no-mouse ~/postgres_describe_table.csv ;
 rm ~/postgres_describe_table.csv ;
 rm ~/table_name.tmp
-# && dw_root -X -t -f table_name.tmp >> ~/postgres_describe_table.csv \
+# && dw_root -X -t -f ~/table_name.tmp >> ~/postgres_describe_table.csv \
