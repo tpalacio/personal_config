826fb9e79626df5b9a7051bbe29f8e697adc006a
Thoren Palacio <tjpalacio@gmail.com>
2025-11-03 16:24:33 -0500

    feat: add missing packages

---
+++ home_dir/dot_emacs.d/elpa/syntax-subword-0.2/syntax-subword-pkg.el
@@ -0,0 +1,9 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "syntax-subword" "0.2"
+  "Make operations on words more fine-grained."
+  ()
+  :url "https://github.com/jpkotta/syntax-subword"
+  :commit "01b27feac37eb60cf73ff80374004d9e847e637d"
+  :revdesc "01b27feac37e"
+  :authors '(("Jonathan Kotta" . "jpkotta@gmail.com"))
+  :maintainers '(("Jonathan Kotta" . "jpkotta@gmail.com")))
